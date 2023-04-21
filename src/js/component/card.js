c5f5c1605c83fe67166e5e54bf3aa0a8aab3f8f0
CatalinaScheleff <catascheleff@gmail.com>
2023-04-21 21:45:29 +0000

    ajustes diseño

--- src/js/component/card.js
+++ src/js/component/card.js
@@ -11,10 +11,10 @@ export const CardPersonajes = () => {
     <div className="container-fluid row justify-content-center d-flex">
       {store.character?.results?.map((character, index) => {
         return (
-          <div className="card col-3 m-2" style={{ width: "18rem" }} key={character.id}>
+          <div className="card text-white col-3 m-2" style={{ width: "18rem", background:"black" }} key={character.id}>
             <img
               src={character.image}
-              className="card-img-top"
+              className="card-img-top mt-2"
               alt={character.name}
             />
             <div className="card-body">
@@ -22,9 +22,9 @@ export const CardPersonajes = () => {
               <p className="card-text">{character.species}</p>
               <div className="botonCarta">
                 <Link to={"/personaje/detalle/" + character.id}>
-                  <button className="btn btn-dark">Más información</button>
+                  <button className="btn btn-info">Más información</button>
                 </Link>
-                <button className="botonCorazon btn btn-dark" 
+                <button className="botonCorazon btn btn-outline-info" 
                 onClick={()=> actions.agregarFavorito(character.name)}>
                   {/* onclick guardar en el store character.name*/}
                   <i className="text-danger fas fa-heart"></i>
@@ -46,19 +46,19 @@ export const CardLocations = () => {
       {store.location?.results?.map((location, index) => {
         return (
           <div
-            className="card col-3 m-2"
-            style={{ width: "18rem" }}
+            className="card col-3 m-2 text-white"
+            style={{ width: "18rem", background: "black"}}
             key={location.id}
           >
-            <img src={planeta} className="card-img-top" alt={location.name} />
+            <img src={planeta} className="card-img-top mt-2" alt={location.name} />
             <div className="card-body">
               <h5 className="card-title">{location.name}</h5>
               <p className="card-text">{location.species}</p>
               <div className="botonCarta">
               <Link to={"/locacion/detalle/" + location.id}>
-                <button className="btn btn-dark">Más información</button>
+                <button className="btn btn-info">Más información</button>
               </Link>
-              <button className="botonCorazon btn btn-dark">
+              <button className="botonCorazon btn btn-outline-info">
                   <i className="text-danger fas fa-heart" 
                   onClick={()=> actions.agregarFavorito(location.name)}></i>
                 </button>
